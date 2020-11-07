5eea68a170dbf68d746d1f566d21f47271512eb7
Pit896 <70265346+Pit896@users.noreply.github.com>
2020-11-07 23:17:49 +0100

    Create achievements.js

---
+++ commands/fall guys/achievements.js
@@ -0,0 +1,32 @@
+const fall = require('fallguys-api');
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+    name: "achievements",
+    category: "Fall Guys",
+    description: "Send you An achievements info",
+    usage: "f!achievements <name_achievements>",
+    aliases: ['ac', 'a'],
+    run: async (client, message, args) => {
+
+        try {
+            if(!args.join(" ")) return message.channel.send(`Unknow Search.`);
+
+            const el = await fall.getAchievements();
+
+            let e = await el.getAchievement("Fall Bae");
+    
+            let embed = new MessageEmbed()
+            .setFooter(`Requested by | ${message.author.username}`, message.author.displayAvatarURL())
+            .setThumbnail(e.icon)
+            .setColor("#ff00d4")
+            .setDescription(e.description)
+            .setTitle(e.name)
+            .setURL(e.icon)
+    
+            message.channel.send(embed);
+        } catch {
+            return message.channel.send("Invalid achievements!").then(msg => msg.delete({ timeout: 5000 }));
+        }
+    }
+}
