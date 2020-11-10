c172296f111b51f3d296a99c5146a3363371b9fe
Pit896 <70265346+Pit896@users.noreply.github.com>
2020-11-10 19:08:42 +0100

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,8 +1,17 @@
+const Guild = require('../database/models/prefix.js');
+
 module.exports = (client) => {
-    client.on('ready', async () => {
+       client.on('ready', async () => {
+        
+       const settings = await Guild.findOne({
+           guildID: message.guild.id
+       }, (err, guild) => {
+           if(err) console.log(err);
+       });
+           
         console.log(`${client.user.username} Ready!`);
         let memberCount = client.guilds.cache.map(guild => guild.memberCount).reduce((a, b) => a + b, 0)
-        client.user.setActivity(`FallGuys Bot | f!help 1.0.1 | In ${client.guilds.cache.size} server and with ${memberCount} users`, {
+        client.user.setActivity(`FallGuys Bot | ${settings.prefix} 1.0.1 | In ${client.guilds.cache.size} server and with ${memberCount} users`, {
             type: 'PLAYING'
         });
     });
