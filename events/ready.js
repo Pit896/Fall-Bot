533cbfa635cab2a282de3c7d5d8ac71a9a25cbd2
Pit896 <70265346+Pit896@users.noreply.github.com>
2020-11-17 16:00:47 +0100

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,12 +1,9 @@
 module.exports = (client) => {
-       client.on('ready', async () => {
-           
+    client.on('ready', async () => {
         console.log(`${client.user.username} Ready!`);
-        let memberCount = client.guilds.cache.map(guild => guild.memberCount).reduce((a, b) => a + b, 0)
-        client.user.setActivity(`FallGuys Bot | f!help 1.0.1 | In ${client.guilds.cache.size} server and with ${memberCount} users`, {
-            type: 'PLAYING'
-        });
+        let memberCount = client.guilds.cache.map(guild => guild.memberCount).reduce((a, b) => a + b, 0)  
+        client.user.setPresence({ activity: { name: `FallGuys Bot | f!help 1.0.1 | In ${client.guilds.cache.size} server and with ${memberCount} users`, type: 'STREAMING', url: 'https://www.twitch.tv/smash690' }});    
     });
-
     client.login(process.env.TOKEN);
 }
+   
