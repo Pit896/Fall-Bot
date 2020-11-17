4527a6b5ad3a4411f3b5d1f8f9eddd02485ebc61
Pit896 <70265346+Pit896@users.noreply.github.com>
2020-11-17 11:44:46 +0100

    Add files via upload

---
+++ commands/bot/uptime.js
@@ -0,0 +1,23 @@
+const { MessageEmbed } = require('discord.js');
+const ms = require('ms');
+
+module.exports = {
+    name: "uptime",
+    category: "Bot",
+    description: "Bot uptime",
+    usage: "f!uptime",
+    run: async (client, message, args) => {
+
+        message.channel.send('Calculating Uptime...').then(m => m.delete({ timeout: 2000 }))
+        .then(resultMessage => {
+            const uptime = ms(client.uptime, { long: true })
+
+            let embed = new MessageEmbed()
+            .setColor("RANDOM")
+            .setDescription(`🖤Uptime: **${uptime}**`)
+
+
+            message.channel.send(embed);
+        });
+    }
+}
