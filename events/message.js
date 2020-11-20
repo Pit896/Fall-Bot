2c8649550eda73bc618be146d58064b842a0c639
Pit896 <70265346+Pit896@users.noreply.github.com>
2020-11-20 08:12:03 +0100

    Update message.js

--- events/message.js
+++ events/message.js
@@ -1,6 +1,4 @@
 const mongoose = require('mongoose');
-const xptables = require('../database/models/users');
-const num = 150
 const Guild = require('../database/models/prefix');
 const discord = require('discord.js');
 const fall = require('fall-guys-api-fixed');
@@ -50,62 +48,7 @@ module.exports = async (client) => {
     
         if (command) 
             command.run(client, message, args);        
-
-            let amount = Math.ceil(Math.random() * 16);     
-
-            let user = await xptables.findOne({
-                serverID: message.guild.id,
-                userID: message.author.id
-            });
-
-            if(message.content.startsWith(prefix + 'tier')) {
-                let embed = new discord.MessageEmbed()
-                .setTitle(`${message.author.username}#${message.author.discriminator} Tier Card`)
-                .setAuthor("Season 1")
-                .setColor('RED')
-                .setThumbnail(message.author.displayAvatarURL({dynamic: true}))
-                if(user) {
-                    let diff = (user.level * num) - user.exp;
-                    embed
-                    .addField(`Tier ${emoji("770670042060161074")}`, user.level)
-                    .addField(`XP`, user.exp)
-                    .setFooter(`Missing ${diff} XP for get the next Tier.`, message.author.displayAvatarURL({dynamic: true}))
-                } else {
-                    embed
-                    .addField(`Tier ${emoji("770670042060161074")}`, "1")
-                    .addField(`XP`, "0")
-                    .setFooter(`Missing ${num} XP for get the next Tier.`, message.author.displayAvatarURL({dynamic: true}))                   
-                }
-                message.channel.send(embed);
-            }
-    
-            if(!user) {
-                await new xptables({
-                    serverID: message.guild.id,
-                    userID: message.author.id,
-                    exp: amount,
-                    level: 1
-                }).save();
-            } else {
-                let nextlevelxp = user.level * num;
-                if(user.exp >= nextlevelxp) {
-                    user.level = user.level- + -1;
-                    user.exp = 0;
-                    user.save();
-                    let embed = new discord.MessageEmbed()
-                    .setColor('BLUE')
-                    .setThumbnail()
-                    .setTimestamp("https://miro.medium.com/max/700/1*ULyYA9TQ64rYrlMVxAuLiw.jpg")
-                    .setTitle("🌠 CONGRATULATION 🌠")
-                    .setFooter(`Reached level ${user.level}!`)
-                    .setDescription(`Congratulations. You have reached level **${user.level}**\nCan you get level ${user.level + 1}?`);
-    
-                    message.channel.send(embed);
-                } else {
-                    user.exp = user.exp- + -amount;
-                    user.save();
-                }
-            }
+      
             function emoji (id) {
                 return client.emojis.cache.get(id).toString();
             }
