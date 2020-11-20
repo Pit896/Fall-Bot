30b176c8ae9c5eaf354839d1023b8cc4d4e7216e
Pit896 <70265346+Pit896@users.noreply.github.com>
2020-11-20 14:48:58 +0100

    Add files via upload

---
+++ commands/general/channel.js
@@ -0,0 +1,26 @@
+const { MessageEmbed } = require('discord.js');
+
+
+module.exports = {
+    name: 'channelinfo',
+    category: 'General',
+    desciption: 'Send you a server channel info',
+    usage: 'f!channelinfo <#channelname>',
+    aliases: ['channel'],
+    run: async (client, message, args) => {
+        const channel = message.mentions.channels.first() || message.channel  
+       
+        const channelEmbed = new MessageEmbed()
+                .setColor('#00E4FF')
+                .setThumbnail(message.guild.iconURL({ dynamic: true }))
+                .setTitle('Channel Info')
+                .addField(':arrow_right: Name', channel.type === 'dm' ? `<@${channel.recipient.username}>` : channel.name, true)
+                .addField(':arrow_right: ID', channel.id, true)
+                .addField(':arrow_right: Creation Date', channel.createdAt.toDateString(), true)
+                .addField(':arrow_right: NSFW', channel.nsfw ? 'Yes' : 'No', true)
+                .addField(':arrow_right: Category', channel.parent ? channel.parent.name : 'None', true)
+                .addField(':arrow_right: Topic', channel.topic || 'None', true);
+     
+        message.channel.send(channelEmbed);
+    }
+}
