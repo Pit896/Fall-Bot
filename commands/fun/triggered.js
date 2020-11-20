2c111e0f3d951ab66d984492f2531d860680a00e
Pit896 <70265346+Pit896@users.noreply.github.com>
2020-11-20 12:53:09 +0100

    Add files via upload

---
+++ commands/fun/triggered.js
@@ -0,0 +1,20 @@
+const canvacord = require('canvacord');
+const { MessageAttachment } = require('discord.js');
+
+
+module.exports = {
+    name: 'triggered',
+    category: 'Fun',
+    description: 'Triggering (new verb [;)',
+    usage: 'f!triggered',
+    run: async (client, message, args) => {
+
+        const me = message.author;
+
+        let data = canvacord.Canvas.trigger(me.displayAvatarURL({ dynamic: false, format: "png" })).then(buffer => {
+
+            let att = new MessageAttachment(buffer, "triggered.gif");
+            message.channel.send(att)
+        });
+    }
+}
