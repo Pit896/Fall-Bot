8c7544887c483cdb615b34e66f20d4e049a7c177
Pit896 <70265346+Pit896@users.noreply.github.com>
2020-11-20 11:17:47 +0100

    Update stop.js

--- commands/music/stop.js
+++ commands/music/stop.js
@@ -8,7 +8,7 @@ module.exports = {
     usage: 'f!stop',
     run: async (client, message, args) => {
         const channel = message.member.voice.channel
-        if (!channel)return sendError("You need to be in a voice channel to play music!", message.channel);
+        if (!channel)return sendError("You need to be in a voice channel to stop music!", message.channel);
         const serverQueue = message.client.queue.get(message.guild.id);
         if (!serverQueue)return sendError("There is nothing playing that I could stop for you :(", message.channel);
         serverQueue.songs = [];
