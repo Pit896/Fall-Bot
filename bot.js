45d3c256bc2f2f5d382b00ba7a0c5f8a18c4cb85
Pit896 <70265346+Pit896@users.noreply.github.com>
2020-11-22 12:38:34 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,49 @@
+const discord = require('discord.js');
+const client = new discord.Client({
+    disableEveryone: true
+});
+const { readdir } = require('fs');
+const config = require('./config/config.json');
+
+require('dotenv').config();
+const DBL = require('dblapi.js');
+const dbl = new DBL(process.env.DBL, client);
+
+
+client.commands = new discord.Collection();
+client.aliases = new discord.Collection();
+client.colors = config.colors;
+client.emotes = config.emotes;
+client.queue = new Map();
+
+readdir('./events/', async (err, files) => {
+    if(err) console.log(err.stack)
+    files.forEach(async file => {
+        if(!file.endsWith(".js")) return;
+        const events = require(`./events/${file}`)(client);
+    });
+});
+
+readdir('./handlers/', async (err, files) => {
+    if(err) console.log(err.stack)
+    files.forEach(file => {
+        if(!file.endsWith(".js")) return;
+        const handlers = require(`./handlers/${file}`)(client);
+    });
+});
+
+readdir('./database/', async (err, files) => {
+    if(err) return console.log(err.stack);
+    files.forEach(file => {
+        if(!file.endsWith(".js")) return;
+        const database = require(`./database/${file}`)(client);
+    });
+});
+
+readdir('./DBL/', async (err, files) => {
+    if(err) return console.log(err.stack);
+    files.forEach(file => {
+        if(!file.endsWith(".js")) return;
+        const DBL = require(`./DBL/${file}`)(client);
+    });
+});
