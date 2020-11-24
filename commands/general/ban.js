f7a771ecf950fbdf9b8e90a7b1c33b58b8fcb557
Pit896 <70265346+Pit896@users.noreply.github.com>
2020-11-24 12:46:19 +0100

    Add files via upload

---
+++ commands/general/ban.js
@@ -0,0 +1,33 @@
+const { MessageEmbed } = require('discord.js');
+
+
+module.exports = {
+    name: 'ban',
+    category: 'General',
+    description: 'Ban a member from the server',
+    usage: 'f!ban <member>',
+    run: async (client, message, args) => {
+
+        if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send(`You don't have \`BAN MEMBERS\` permission to do that!`)
+        let member = message.mentions.members.first() || message.guild.members.cache.get(args[0])
+        if(!member) return message.channel.send("Please specify a member to ban.");
+        let reason;
+        reason = args.slice(1).join(" ") || 'None';
+
+        if(!message.guild.me.hasPermission("BAN_MEMBERS")) {
+            return message.channel.send("I don't have permission to ban!");
+        }
+
+        if(member.hasPermission("ADMINISTRATOR")) return message.channel.send(`I can't ban ${member.user.username}!`) 
+
+        let embed = new MessageEmbed()
+        .setColor("RED")
+        .setTitle("Banned From Server: " + message.guild.name)
+        .setThumbnail(member.user.displayAvatarURL({ dynamic: true }))
+        .setAuthor(`Action: Ban by ${message.author.username}`, message.author.displayAvatarURL({ dynamic: true }))
+        .setDescription(`Banned **${member.user.username}** by **${message.author.username}**\nReason: ${reason}`)
+
+        member.ban({ reason: reason });
+        message.channel.send(embed);
+    }
+}
