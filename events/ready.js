31c9449ced545ffdb72b59c9ce49f46eafb98daf
Pit896 <70265346+Pit896@users.noreply.github.com>
2020-11-24 19:37:30 +0100

    Update ready.js for version bot 1.1.0

--- events/ready.js
+++ events/ready.js
@@ -1,8 +1,8 @@
 module.exports = (client) => {
     client.on('ready', async function() {
         console.log(`${client.user.username} Ready!`);
-        let memberCount = client.guilds.cache.map(guild => guild.memberCount).reduce((a, b) => a + b, 0)  
-        client.user.setPresence({ activity: { name: `FallGuys Bot | f!help 1.0.1 | In ${client.guilds.cache.size} server and with ${memberCount} users`, type: 'STREAMING', url: 'https://www.twitch.tv/smash690' }});    
+//        let memberCount = client.guilds.cache.map(guild => guild.memberCount).reduce((a, b) => a + b, 0)  
+        client.user.setPresence({ activity: { name: `FallGuys Bot | f!help 1.1.0 | 🔮New Leveling System`, type: 'STREAMING', url: 'https://www.twitch.tv/smash690' }});    
     });
     client.login(process.env.TOKEN);
 }
