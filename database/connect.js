5d004c04f95a67596e23440e1c73bc1d9fa37b51
Pit896 <70265346+Pit896@users.noreply.github.com>
2020-11-24 17:53:17 +0100

    Update connect.js

--- database/connect.js
+++ database/connect.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 console.log("Connecting...");
 
 module.exports = async (client) => {
-    await mongoose.connect('mongodb+srv://Fall_bot:Fall_Bot@cluster0.l64gy.mongodb.net/FallBot?retryWrites=true&w=majority', {
+    await mongoose.connect(process.env.MONGO, {
         useNewUrlParser: true,
         useUnifiedTopology: true
     }).then(console.log("Connected to Database Fall bot"))
