aa5804d98d8cd7bfaf6fdec7abd353401b3bc88c
Pit896 <70265346+Pit896@users.noreply.github.com>
2020-11-24 22:31:00 +0100

    Add files via upload

---
+++ commands/fun/ascii.js
@@ -0,0 +1,29 @@
+const figlet = require('figlet');
+
+module.exports = {
+    name: 'ascii',
+    category: 'Fun',
+    description: 'Text Generator!',
+    usage: 'f!ascii <text>',
+    run: async (client, message, args) => {
+
+        if(!args.join(" ")) return message.channel.send("To Text Provided!");
+        figlet.text(args.join(' '), {
+            font: 'Standard',
+            horizontalLayout: 'default',
+            verticalLayout: 'default'
+        }, async (err, render) => {
+            if(err) {
+                console.log(err.stack)
+                message.channel.send('Invalid Text!');    
+            }
+
+            if(render.length > 2000) return message.channel.send("The Text Is so Long");
+            if(render.trim().length === 0) return message.channel.send('Invalid Text!'); 
+
+
+            message.channel.send("Here is your ascii")
+            message.channel.send(render, { code: "md" });
+        });
+    }
+}
