b9824fb8a59bda467ec3b16c885bb11bc4fdd825
Pit896 <70265346+Pit896@users.noreply.github.com>
2020-11-25 08:27:49 +0100

    (;

---
+++ commands/fun/cookie.js
@@ -0,0 +1,24 @@
+const fetch = require('node-fetch');
+const { MessageEmbed } = require('discord.js');
+
+
+module.exports = {
+    name: 'cookie',
+    category: 'Fun',
+    description: 'Fortune cookie tip',
+    usage: 'f!cookie',
+    run: async (client, message, args) => {
+
+        try {
+            const res = await fetch('http://yerkee.com/api/fortune');
+            const json = await res.json();
+            const embed = new MessageEmbed()
+              .setColor('RANDOM')
+              .setTitle('Fortune Cookie')
+              .setDescription(json.fortune);
+            return message.channel.send(embed);
+        } catch {
+            message.channel.send('Couldn\' t obtain fortune cookie!');
+        }
+    }
+}
