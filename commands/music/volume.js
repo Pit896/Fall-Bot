0001cffb8ae95a38700fc9cfdcf49d8b79c5aeaa
Pit896 <70265346+Pit896@users.noreply.github.com>
2020-11-27 22:45:55 +0100

    Update volume.js

--- commands/music/volume.js
+++ commands/music/volume.js
@@ -5,10 +5,10 @@ module.exports = {
     name: 'volume',
     description: 'Set the music volume',
     category: 'Music',
-    usage: 'volume <1/10>',
+    usage: 'volume <1/100>',
     run: async (client, message, args) => {
         if(!args[0]) return sendError("Please specify the volume", message.channel);
-        if(args[0] > 10) return sendError("Please a number between 1 and 10", message.channel);
+        if(args[0] > 100) return sendError("Please a number between 1 and 10", message.channel);
         if(args[0] < 0) return sendError("Please a number between 1 and 10", message.channel);
 
         const channel = message.member.voice.channel;
@@ -17,9 +17,9 @@ module.exports = {
         if (!serverQueue) return sendError("There is nothing playing.", message.channel);
         if (!args[0])return message.channel.send(`The current volume is: **${serverQueue.volume}**`);
         serverQueue.volume = args[0]; 
-        serverQueue.connection.dispatcher.setVolumeLogarithmic(args[0] / 5);
+        serverQueue.connection.dispatcher.setVolumeLogarithmic(args[0] / 50);
         let xd = new MessageEmbed()
-        .setDescription(`I set the volume to: **${args[0]}/10**`)
+        .setDescription(`I set the volume to: **${args[0]}/100**`)
         .setAuthor("Server Volume Manager", "https://raw.githubusercontent.com/SudhanPlayz/Discord-MusicBot/master/assets/Music.gif")
         .setColor("BLUE")
         return message.channel.send(xd);
