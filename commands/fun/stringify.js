e9c2eee696315d5f2c0b60e9adc32cb0755bcf74
Pit896 <70265346+Pit896@users.noreply.github.com>
2020-11-28 09:19:50 +0100

    Add files via upload

---
+++ commands/fun/stringify.js
@@ -0,0 +1,24 @@
+const { Random } = require('something-random-on-discord');
+const random = new Random();
+
+module.exports = {
+    name: 'stringify',
+    category: 'Fun',
+    description: 'Stringify',
+    usage: 'f!stringify | f!stringify <number>',
+    run: async (client, message, args) => {
+
+        if(!args[0]) {
+            let data = await random.getString(10);
+
+            message.channel.send(`\`\`\`${data}\`\`\``);
+        } else {
+            if(isNaN(args[0])) return message.channel.send(`\`${args[0]}\` isn't a number!`);
+            if(args[0] == 0) return message.channel.send("I can't send you nothing!");
+                let data = await random.getString(args[0]);
+
+                if(data === undefined) return message.channel.send("The number is too long...");
+                message.channel.send(`\`\`\`${data}\`\`\``);
+        }
+    }
+}
