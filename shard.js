cc6bc0a0b68623618a161b31a65f3bf43ae33f3f
Pit896 <70265346+Pit896@users.noreply.github.com>
2020-12-04 15:22:44 +0100

    Add files via upload

---
+++ shard.js
@@ -0,0 +1,30 @@
+const { ShardingManager } = require('discord.js');
+require('dotenv').config();
+
+const shards = new ShardingManager('./bot.js', {
+    token: process.env.TOKEN,
+    totalShards: "auto"
+});
+
+shards.on('shardCreate', shard => {
+    console.log(`[${new Date().toString().split(" ", 5).join(" ")}] Launching shard #${shard.id}...`);
+    shard.on('ready', () => {
+        console.log(`Shard #${shard.id} Spawned!`);
+    });
+
+    shard.on('error', err => {
+        console.log(`Shard Error ${err.stack}!`);
+    });
+
+    shard.on('reconnecting', () => {
+        console.log("Shard loose connection");
+    });
+
+    shard.on('spawn', spawn => {
+        spawn.on('disconnect', () => {
+            console.log("Shard Disconnected!")
+        });
+    });
+});
+
+shards.spawn(shards.totalShards, 10000)
