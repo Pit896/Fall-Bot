47d770186933d2a2a2c74c5d26047ad6fbbb1177
Pit896 <70265346+Pit896@users.noreply.github.com>
2020-12-04 10:38:31 +0100

    Add files via upload

---
+++ commands/logger/rdelete.js
@@ -0,0 +1,51 @@
+const model = require('../../database/models/roleDelete');
+
+
+module.exports = {
+    name: 'r-deleter',
+    category: 'Logger',
+    description: 'Manage the log channel of log type: "Role Delete"',
+    usage: 'f!r-deleter <#channel> | f!r-deleter disable',
+    run: async (client, message, args) => {
+
+        if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send(`You don't have \`MANAGE MESSAGES\` permission to do that!`)
+        .then(m => m.delete({ timeout: 10000 }));
+
+        let Guild = await model.findOne({
+            guildID: message.guild.id,
+            guildName: message.guild.id            
+        }, async (err, guild) => {
+            if(err) console.log(err);
+            if(!guild) {
+                const NewRes = new model({
+                    guildID: message.guild.id,
+                    guildName: message.guild.id,
+                    ChannelID: null
+                });
+                NewRes.save();
+
+                return message.channel.send("You are now in our Database with command `r-deleter`, retype the command!");
+            }
+        });
+
+        if(args[0] == 'disable') {
+            if(Guild.ChannelID === null) {
+                message.channel.send("⚠ The Role Deleter is arleady disabled!");
+            } else {
+                await Guild.updateOne({
+                    ChannelID: null
+                });
+
+                message.channel.send(`🛑 Role Deleter Logger Disabled!`);
+            }
+        } else {
+            const channel = message.mentions.channels.first();
+            if(!channel) return message.channel.send("No Channel Specifited!");
+
+            await Guild.updateOne({
+                ChannelID: channel.id
+            });
+            message.channel.send(`🔰 Seted The Role Deleter in Channel ${channel}`);
+        }
+    }
+}
---
+++ commands/logger/rupdate.js
@@ -0,0 +1,51 @@
+const model = require('../../database/models/roleUpdate');
+
+
+module.exports = {
+    name: 'r-updater',
+    category: 'Logger',
+    description: 'Manage the log channel of log type: "Role Update"',
+    usage: 'f!r-updater <#channel> | f!r-updater disable',
+    run: async (client, message, args) => {
+
+        if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send(`You don't have \`MANAGE MESSAGES\` permission to do that!`)
+        .then(m => m.delete({ timeout: 10000 }));
+
+        let Guild = await model.findOne({
+            guildID: message.guild.id,
+            guildName: message.guild.id            
+        }, async (err, guild) => {
+            if(err) console.log(err);
+            if(!guild) {
+                const NewRes = new model({
+                    guildID: message.guild.id,
+                    guildName: message.guild.id,
+                    ChannelID: null
+                });
+                NewRes.save();
+
+                return message.channel.send("You are now in our Database with command `r-updater`, retype the command!");
+            }
+        });
+
+        if(args[0] == 'disable') {
+            if(Guild.ChannelID === null) {
+                message.channel.send("⚠ The Role Updater is arleady disabled!");
+            } else {
+                await Guild.updateOne({
+                    ChannelID: null
+                });
+
+                message.channel.send(`🛑 Role Updater Logger Disabled!`);
+            }
+        } else {
+            const channel = message.mentions.channels.first();
+            if(!channel) return message.channel.send("No Channel Specifited!");
+
+            await Guild.updateOne({
+                ChannelID: channel.id
+            });
+            message.channel.send(`🔰 Seted The Role Updater in Channel ${channel}`);
+        }
+    }
+}
