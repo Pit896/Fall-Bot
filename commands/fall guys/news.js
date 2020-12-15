9a3e3d743ed28f47f72e08a6ee604291cc1e5d32
Pit896 <70265346+Pit896@users.noreply.github.com>
2020-12-15 17:11:27 +0100

    Update news.js

--- commands/fall guys/news.js
+++ commands/fall guys/news.js
@@ -12,11 +12,11 @@ module.exports = {
 
         let embed = new MessageEmbed()
         .setColor("#33cccc")
-        .setURL("https://fallguys.com/news/season-2-5-drops-today")
-        .setImage("https://www.pcinvasion.com/wp-content/uploads/2020/11/Fall-Guys-Big-Fans-update-800x400.jpg")
+        .setURL("https://fallguys.com/news/season-3-theme-reveal-20-off-sale-golden-joysticks")
+        .setImage("https://fallguys.com/_next/image?url=https%3A%2F%2Fimages.ctfassets.net%2Fvsmfukibxsuz%2F5k7tFPZGllG9xT3g5oXC5A%2Fcb75ec4f8bbcdb9b2fc73f9f303b366c%2FSeason3__1_.jpg&w=1200&q=75")
         .setAuthor("News", client.user.displayAvatarURL())
-        .setTitle("Season 2.5 drops today!")
-        .setDescription("After transporting players into the domain of medieval mayhem for Season 2, we’ve dived back in with a hefty order of round remixes, oodles of community-inspired improvements and the addition of all-new Round, ‘Big Fans’. Come meet the content that’ll keep things fresh and fun in the lead up to Season 3… \n*[Press Here For Read More](https://fallguys.com/news/season-2-5-drops-today)*")
+        .setTitle("Fall Guys Season 3 - Winter Knockout revealed!")
+        .setDescription("The tenacity, inventiveness and puzzle-solving skills of the Fall Guys community have, of course, never been in doubt. But even we were shocked by the lightning pace players threw their collective will into solving __Operation #JigSawus__, revealing the icy cold aesthetic of Fall Guys Season 3 - Winter Knockout! But what in the Blunderdome is Operation #JigSawus..? Well, we decided to shatter some delightful Season 3 art into 300 pieces and spread them far and wide across the __digital plains__ of the Fall Guys fandom! Less than 24 hours later, the community had conquered our conundrum, piecing together the deeply chilly art work now before your eyes. Amazing. We are beyond excited to share more information on the wonders of Fall Guys Season 3 soon...and we strongly recommend tuning into __The Game Awards on December 10th..!__ But for now, please look forward to the slew of snowy shenanigans to come. And the penguin. Just look at that tiny, tiny penguin.\n[Press Here For Read More](https://fallguys.com/news/season-3-theme-reveal-20-off-sale-golden-joysticks)*")
 
         message.channel.send(embed);
     }
